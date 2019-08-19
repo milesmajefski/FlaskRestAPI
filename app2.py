803a523a60f485787eb7bedc42ef444a2b947ecd
Miles <milesmajefski@gmail.com>
2019-08-18 21:06:53 -0500

    boilerplate

---
+++ app2.py
@@ -0,0 +1,15 @@
+"""
+Microblog service as Restful API
+
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
