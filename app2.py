583781dcfe0a4b2bb89d96c1cbe4f4f6be0f714c
Miles <milesmajefski@gmail.com>
2019-09-01 21:30:54 -0500

    added create end point

--- app2.py
+++ app2.py
@@ -39,20 +39,48 @@ www.us.com/posts/1625375
 """
 import os
 import pprint
-from flask import Flask
-from pymongo import MongoClient
+from flask import Flask, Response, request, jsonify
+from pymongo import MongoClient, errors
+from bson import json_util
+
+
 app = Flask(__name__)
 db_user = os.getenv('DB_USER', 'nuthin')
 db_password = os.getenv('DB_PASSWORD', '')
+connect_str = 'mongodb+srv://{}:{}@cluster0-uh9qt.mongodb.net/test?retryWrites=true&w=majority'.format(db_user, db_password)
 
-@app.route('/home')
+
 @app.route('/')
 def index():
-    return "Hello, World!"
+    try:
+        client = MongoClient(connect_str)
+    except errors.PyMongoError as e:
+        return 'Fail! cuz db no worky! %s' % e
+
+    db = client['python_class']
+    if db:
+        posts = db.microblog
+        post = posts.find()
+        return Response(json_util.dumps(post), mimetype='application/json')
+
+
+@app.route('/posts', methods=['POST'])
+def create_post():
+    try:
+        client = MongoClient(connect_str)
+        # return request.form['title']
+    except errors.PyMongoError as e:
+        return 'Fail! cuz db no worky! %s' % e
+
+    db = client['python_class']
+    if db:
+        posts = db.microblog
+        post_data = {'title': request.form['title']}
+        inserted = posts.insert_one(post_data)
+        return Response(json_util.dumps(inserted.inserted_id), mimetype='application/json')
+
 
 def connect():
-    connect_str = 'mongodb+srv://{}:{}@cluster0-uh9qt.mongodb.net/test?retryWrites=true&w=majority'.format(db_user, db_password)
-
     client = MongoClient(connect_str)
     db = client['python_class']
     if db:
@@ -61,6 +89,6 @@ def connect():
         pprint.pprint(posts.find_one())
 
 if __name__ == '__main__':
-    connect()
-    print(db_user)
+    #connect()
+    #print(db_user)
     app.run(debug=False)
